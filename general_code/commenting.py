9fac44c104716d8e064fba15a995df574d04363a
abhishekrathore311 <abhishek.rathore311@gmail.com>
2021-04-04 19:54:40 +0530

    Added books folder

---
+++ general_code/commenting.py
@@ -0,0 +1,16 @@
+def create_is_status_features_for_yearmo(yearmo):
+    """
+    Creates aggregated features for is_status attributes of fact_core_item given yearmo
+    
+    Parameters
+    ----------
+    yearmo: int
+        yearmo of the adopters for which features need to be created
+    
+    Returns
+    -------
+    df_transformed: pandas dataframe
+        dataframe having all the created features for adopters of given yearmo
+    """
+    new_yearmo = yearmo
+    return new_yearmo
